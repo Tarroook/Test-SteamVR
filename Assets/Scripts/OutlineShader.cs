ed0f047ec6e20e34ec4d3d9e00d3919f12d4f164
Tarook <Tarook.Zinou@gmail.com>
2022-11-02 14:37:12 +0100

    Added packages / made an outline shader

---
+++ Assets/Scripts/OutlineShader.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineShader : MonoBehaviour
+{
+    private Mesh mesh;
+    private void Start()
+    {
+        
+    }
+}
---
+++ Assets/StartXR.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Management;
+
+public class StartXR : MonoBehaviour
+{
+    bool startInVr = true;
+    public GameObject vrPrefab;
+    public GameObject flatScreenPrefab;
+    private void Awake()
+    {
+        if (startInVr)
+        {
+            XRGeneralSettings.Instance.Manager.InitializeLoaderSync();
+            XRGeneralSettings.Instance.Manager.StartSubsystems();
+            Instantiate(vrPrefab, Vector3.zero, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(flatScreenPrefab, Vector3.zero, Quaternion.identity);
+        }
+    }
+}
